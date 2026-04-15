9aa22c6c3dfa9fe39a8f53b1f16029df2a16cf9c
Macjim202 <macjim202@gmail.com>
2026-04-15 17:47:58 +0500

    Task1

---
+++ Tyuiu.ZaitsevMS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZaitsevMS.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string name = "Иван";
+            int age = 25;
+
+            Console.WriteLine("Здравствуйте, меня зоовут " + name + " мне " + age " лет.");
+            Console.ReadKey();
+        }
+    }
+}
