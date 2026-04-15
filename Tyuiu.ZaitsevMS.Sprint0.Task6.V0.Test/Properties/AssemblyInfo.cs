9f8a9d2435f968dd22a8a892248c8747672b58d1
Macjim202 <macjim202@gmail.com>
2026-04-15 18:58:55 +0500

    Task6

---
+++ Tyuiu.ZaitsevMS.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZaitsevMS.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZaitsevMS.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d31cca4-7c3e-4e5c-b9dd-de829925fc68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZaitsevMS.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ZaitsevMS.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.ZaitsevMS.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма эл. = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность эл. = " + DataService.SubstractArray(numsArray));
+            Console.WriteLine("Произведение эл. = " + DataService.MultArray(numsArray));
+            Console.ReadKey();
+        }
+    }
+}
