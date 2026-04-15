367373c6580148c8582aabc8ec9b4d8a0ce57f14
Macjim202 <macjim202@gmail.com>
2026-04-15 18:37:41 +0500

    Task5

---
+++ Tyuiu.ZaitsevMS.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZaitsevMS.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.ZaitsevMS.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(2, DataService.Division(10, 5));
+        }
+    }
+}
---
+++ Tyuiu.ZaitsevMS.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZaitsevMS.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZaitsevMS.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad7dde3d-4a61-4c29-8840-e61cc25243c1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
